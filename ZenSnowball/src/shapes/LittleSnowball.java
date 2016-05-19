4bf1b26fc98b16dd91ac014aace876a566eb7455
Waveley Qiu <WaveleyQiu@Waveleys-MacBook-Air.local>
2016-05-19 08:38:09 -0700

    Added LittleSnowball class

---
+++ ZenSnowball/src/shapes/LittleSnowball.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class LittleSnowball extends Snowball{
+
+	public LittleSnowball(float x, float y, float z, float r) {
+		super(x, y, z, r);
+		// TODO Auto-generated constructor stub
+	}
+
+}
