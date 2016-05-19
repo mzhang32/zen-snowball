f81355e4066f2a230906e7e1e2018b9ee9b99931
Waveley Qiu <WaveleyQiu@waveleys-air.attlocal.net>
2016-05-18 21:49:09 -0700

    Adding BigSnowball class

---
+++ ZenSnowball/src/shapes/BigSnowball.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class BigSnowball extends Snowball implements Drawable{
+
+	public BigSnowball(float x, float y, float z, float r) {
+		super(x, y, z, r);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void act(Path path) {
+		
+			}
+		
+		// TODO Auto-generated method stub
+		
+	}
+
